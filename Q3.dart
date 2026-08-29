0cbde8080c260c500842a8aba925a60bc4a14375
ahmed mostafa lokman <ahmadmostafalokman@gmail.com>
2026-08-29 23:04:10 +0400

    Add files via upload

---
+++ Q3.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main (){
+  print("enter a number you want to make it constant:");
+  final num=stdin.readLineSync();
+
+
+print("the number has set as constant:$num");
+
+
+
+}
